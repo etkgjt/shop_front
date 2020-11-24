3883603383d5d2865fe1aea6da7d7c09ea1934ed
etkgjt <canlong102@gmail.com>
2020-11-24 10:36:16 +0700

    update

--- front-end/src/untils/socket.js
+++ front-end/src/untils/socket.js
@@ -1,4 +1,4 @@
 import io from 'socket.io-client';
-const socket = io('https://cn-web-socket.herokuapp.com');
+const socket = io('https://cn-web-socket.herokuapp.com/');
 console.log('socket connect ne', socket);
 export default socket;
